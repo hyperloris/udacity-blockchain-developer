1a75dd0814e758b57db916e1b071fb5a38355423
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2021-02-18 10:53:29 +0100

    chore(supply-chain): add setup for rinkeby deploy

--- 6-supply-chain/project-6/truffle.js
+++ 6-supply-chain/project-6/truffle.js
@@ -1,9 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "f534e9ec8b504e55a2a5a144caa4dc08";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 4500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+    } 
   }
 };
