d2796ea731a76b0d98d74999ce30b0aab33fdd32
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2021-06-29 17:59:05 +0200

    feat(capstone): implement Ownable contract

--- capstone/eth-contracts/contracts/ERC721Mintable.sol
+++ capstone/eth-contracts/contracts/ERC721Mintable.sol
@@ -7,17 +7,28 @@ import 'openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol';
 import "./Oraclize.sol";
 
 contract Ownable {
-    //  TODO's
-    //  1) create a private '_owner' variable of type address with a public getter function
-    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
-    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
-    //  4) fill out the transferOwnership function
-    //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    address private _owner;
+
+    event OwnershipTransferred(address previousOwner, address newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Only owner can call");
+    }
+
+    constructor() internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
 
     function transferOwnership(address newOwner) public onlyOwner {
-        // TODO add functionality to transfer control of the contract to a newOwner.
-        // make sure the new owner is a real address
-
+        require(newOwner == address(newOwner), "Invalid address");
+        address previousOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(previousOwner, _owner);
     }
 }
 
