681c3f814fa7ec5eda6b83928792584de374efe6
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2021-07-11 12:03:47 +0200

    chore(capstone): add configs for rinkeby deploy

--- capstone/eth-contracts/truffle-config.js
+++ capstone/eth-contracts/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWallet = require('@truffle/hdwallet-provider');
+const infuraKey = "f534e9ec8b504e55a2a5a144caa4dc08";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync('../.secret').toString().trim();
 
 module.exports = {
   /**
@@ -47,6 +47,17 @@ module.exports = {
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
+     rinkeby: {
+      provider: () => new HDWallet(mnemonic, `wss://rinkeby.infura.io/ws/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      websocket: true,
+      timeoutBlocks: 50000,
+      networkCheckTimeout: 1000000
+    },
 
     // Another network with more advanced options...
     // advanced: {
